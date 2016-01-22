6d47235d77e1ac9366e83dfaec204b98285f8f8c
Emmanuel Keller <ekeller@open-search-server.com>
2016-01-22 17:25:31 +0100

    - Improved service implementation - Libraries updates

--- src/main/java/com/qwazr/database/TableServiceInterface.java
+++ src/main/java/com/qwazr/database/TableServiceInterface.java
@@ -19,7 +19,8 @@ import com.qwazr.database.model.ColumnDefinition;
 import com.qwazr.database.model.TableDefinition;
 import com.qwazr.database.model.TableRequest;
 import com.qwazr.database.model.TableRequestResult;
-import com.qwazr.utils.server.RestApplication;
+import com.qwazr.utils.server.ServiceInterface;
+import com.qwazr.utils.server.ServiceName;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.*;
@@ -31,87 +32,88 @@ import java.util.Set;
 
 @RolesAllowed(TableManager.SERVICE_NAME_TABLE)
 @Path("/table")
-public interface TableServiceInterface {
+@ServiceName(TableManager.SERVICE_NAME_TABLE)
+public interface TableServiceInterface extends ServiceInterface {
 
 	@GET
 	@Path("/")
-	@Produces(RestApplication.APPLICATION_JSON_UTF8)
+	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	Set<String> list(@QueryParam("timeout") Integer msTimeOut, @QueryParam("local") Boolean local);
 
 	@POST
 	@Path("/{table_name}")
-	@Consumes(RestApplication.APPLICATION_JSON_UTF8)
-	@Produces(RestApplication.APPLICATION_JSON_UTF8)
+	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
+	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	TableDefinition createTable(@PathParam("table_name") String table_name);
 
 	@GET
 	@Path("/{table_name}")
-	@Produces(RestApplication.APPLICATION_JSON_UTF8)
+	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	TableDefinition getTable(@PathParam("table_name") String table_name);
 
 	@DELETE
 	@Path("/{table_name}")
-	@Produces(RestApplication.APPLICATION_JSON_UTF8)
+	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	Boolean deleteTable(@PathParam("table_name") String table_name);
 
 	@GET
 	@Path("/{table_name}/column")
-	@Produces(RestApplication.APPLICATION_JSON_UTF8)
+	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	Map<String, ColumnDefinition> getColumns(@PathParam("table_name") String table_name);
 
 	@GET
 	@Path("/{table_name}/column/{column_name}")
-	@Produces(RestApplication.APPLICATION_JSON_UTF8)
+	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	ColumnDefinition getColumn(@PathParam("table_name") String table_name,
-			@PathParam("column_name") String column_name);
+					@PathParam("column_name") String column_name);
 
 	@POST
 	@Path("/{table_name}/column/{column_name}")
-	@Consumes(RestApplication.APPLICATION_JSON_UTF8)
-	@Produces(RestApplication.APPLICATION_JSON_UTF8)
+	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
+	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	ColumnDefinition addColumn(@PathParam("table_name") String table_name, @PathParam("column_name") String column_name,
-			ColumnDefinition columnDefinition);
+					ColumnDefinition columnDefinition);
 
 	@DELETE
 	@Path("/{table_name}/column/{column_name}")
-	@Produces(RestApplication.APPLICATION_JSON_UTF8)
+	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	Boolean removeColumn(@PathParam("table_name") String table_name, @PathParam("column_name") String column_name);
 
 	@POST
 	@Path("/{table_name}/row")
-	@Consumes(RestApplication.APPLICATION_JSON_UTF8)
-	@Produces(RestApplication.APPLICATION_JSON_UTF8)
+	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
+	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	Long upsertRows(@PathParam("table_name") String table_name, List<Map<String, Object>> rows);
 
 	@POST
 	@Path("/{table_name}/row")
 	@Consumes(MediaType.TEXT_PLAIN)
-	@Produces(RestApplication.APPLICATION_JSON_UTF8)
+	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	Long upsertRows(@PathParam("table_name") String table_name, @QueryParam("buffer") Integer buffer,
-			InputStream inpustStream);
+					InputStream inpustStream);
 
 	@PUT
 	@Path("/{table_name}/row/{row_id}")
-	@Consumes(RestApplication.APPLICATION_JSON_UTF8)
-	@Produces(RestApplication.APPLICATION_JSON_UTF8)
+	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
+	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	Map<String, Object> upsertRow(@PathParam("table_name") String table_name, @PathParam("row_id") String row_id,
-			Map<String, Object> node);
+					Map<String, Object> node);
 
 	@GET
 	@Path("/{table_name}/row/{row_id}")
-	@Produces(RestApplication.APPLICATION_JSON_UTF8)
+	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	Map<String, Object> getRow(@PathParam("table_name") String table_name, @PathParam("row_id") String row_id,
-			@QueryParam("column") Set<String> columns);
+					@QueryParam("column") Set<String> columns);
 
 	@DELETE
 	@Path("/{table_name}/row/{row_id}")
-	@Produces(RestApplication.APPLICATION_JSON_UTF8)
+	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	Boolean deleteRow(@PathParam("table_name") String table_name, @PathParam("row_id") String row_id);
 
 	@POST
 	@Path("/{table_name}/query")
-	@Consumes(RestApplication.APPLICATION_JSON_UTF8)
-	@Produces(RestApplication.APPLICATION_JSON_UTF8)
+	@Consumes(ServiceInterface.APPLICATION_JSON_UTF8)
+	@Produces(ServiceInterface.APPLICATION_JSON_UTF8)
 	TableRequestResult queryRows(@PathParam("table_name") String graph_name, TableRequest request);
 
 }
