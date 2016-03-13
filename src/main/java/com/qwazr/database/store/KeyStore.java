e3fb5da2ff8f3e94b84214c541176a2236de339a
Emmanuel Keller <ekeller@open-search-server.com>
2016-03-13 23:43:25 +0100

    Improvements

--- src/main/java/com/qwazr/database/store/KeyStore.java
+++ src/main/java/com/qwazr/database/store/KeyStore.java
@@ -15,32 +15,22 @@
  **/
 package com.qwazr.database.store;
 
-import com.qwazr.utils.LockUtils;
 import org.fusesource.leveldbjni.JniDBFactory;
 import org.iq80.leveldb.DB;
 import org.iq80.leveldb.DBIterator;
 import org.iq80.leveldb.Options;
-import org.iq80.leveldb.WriteBatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class KeyStore implements Closeable {
-
-	public static final int MAX_WRITE_CACHE_SIZE = 50000;
 
 	private static final Logger logger = LoggerFactory.getLogger(KeyStore.class);
 
 	private final DB db;
-
-	private static final byte[] deletedKey = new byte[0];
-
-	private final LockUtils.ReadWriteLock rwlMemory = new LockUtils.ReadWriteLock();
-	private final ConcurrentHashMap<byte[], byte[]> memoryTemp = new ConcurrentHashMap<byte[], byte[]>();
 
 	public KeyStore(File file) throws IOException {
 		Options options = new Options();
@@ -49,70 +39,23 @@ public class KeyStore implements Closeable {
 	}
 
 	public void commit() throws IOException {
-		rwlMemory.r.lock();
-		try {
-			if (logger.isDebugEnabled())
-				logger.debug("Write batch: " + memoryTemp.size());
-			WriteBatch batch = db.createWriteBatch();
-			try {
-				memoryTemp.forEach((key, value) -> {
-					if (key == deletedKey)
-						batch.delete(key);
-					else
-						batch.put(key, value);
-				});
-				db.write(batch);
-				memoryTemp.clear();
-			} finally {
-				batch.close();
-			}
-		} finally {
-			rwlMemory.r.unlock();
-		}
 	}
 
 	@Override
 	public void close() throws IOException {
-		commit();
 		db.close();
 	}
 
 	public byte[] get(byte[] key) {
-		rwlMemory.r.lock();
-		try {
-			byte[] value = memoryTemp.get(key);
-			if (value == deletedKey)
-				return null;
-			if (value != null)
-				return value;
-			return db.get(key);
-		} finally {
-			rwlMemory.r.unlock();
-		}
+		return db.get(key);
 	}
 
 	public void put(byte[] key, byte[] value) throws IOException {
-		rwlMemory.w.lock();
-		try {
-			if (value == null || value.length == 0)
-				value = deletedKey;
-			memoryTemp.put(key, value);
-		} finally {
-			rwlMemory.w.unlock();
-		}
-		if (memoryTemp.size() >= MAX_WRITE_CACHE_SIZE)
-			commit();
+		db.put(key, value);
 	}
 
 	public void delete(byte[] key) throws IOException {
-		rwlMemory.w.lock();
-		try {
-			memoryTemp.put(key, deletedKey);
-		} finally {
-			rwlMemory.w.unlock();
-		}
-		if (memoryTemp.size() >= MAX_WRITE_CACHE_SIZE)
-			commit();
+		db.delete(key);
 	}
 
 	public DBIterator iterator() {
--- src/main/java/com/qwazr/database/store/keys/KeysAbstract.java
+++ src/main/java/com/qwazr/database/store/keys/KeysAbstract.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015-2016 Emmanuel Keller / QWAZR
- * <p/>
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,26 +24,26 @@ import java.util.Map;
 
 public abstract class KeysAbstract<T> extends KeyAbstract<T> {
 
-    public KeysAbstract(KeyEnum keyType) {
-	super(keyType, null);
-    }
-
-    final public int deleteAll(KeyStore store) throws IOException {
-	byte[] myKey = getCachedKey();
-	ByteBuffer bb = ByteBuffer.wrap(myKey);
-	DBIterator iterator = store.iterator();
-	iterator.seek(myKey);
-	int count = 0;
-	while (iterator.hasNext()) {
-	    Map.Entry<byte[], byte[]> entry = iterator.next();
-	    byte[] key = entry.getKey();
-	    if (key.length < myKey.length)
-		break;
-	    if (!bb.equals(ByteBuffer.wrap(key, 0, myKey.length)))
-		break;
-	    store.delete(key);
-	    count++;
+	public KeysAbstract(KeyEnum keyType) {
+		super(keyType, null);
 	}
-	return count;
-    }
+
+	final public int deleteAll(KeyStore store) throws IOException {
+		byte[] myKey = getCachedKey();
+		ByteBuffer bb = ByteBuffer.wrap(myKey);
+		DBIterator iterator = store.iterator();
+		iterator.seek(myKey);
+		int count = 0;
+		while (iterator.hasNext()) {
+			Map.Entry<byte[], byte[]> entry = iterator.next();
+			byte[] key = entry.getKey();
+			if (key.length < myKey.length)
+				break;
+			if (!bb.equals(ByteBuffer.wrap(key, 0, myKey.length)))
+				break;
+			store.delete(key);
+			count++;
+		}
+		return count;
+	}
 }
