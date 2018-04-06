fe9ee852a09fab1238f31c3001c235a3f8ac0091
Emmanuel Keller <ekeller@open-search-server.com>
2018-04-06 11:49:25 +0200

    Fixes non closed stream when using Files.list

--- src/main/java/com/qwazr/database/TableManager.java
+++ src/main/java/com/qwazr/database/TableManager.java
@@ -15,16 +15,8 @@
  */
 package com.qwazr.database;
 
-import com.qwazr.database.model.ColumnDefinition;
-import com.qwazr.database.model.TableDefinition;
-import com.qwazr.database.model.TableRequest;
-import com.qwazr.database.model.TableRequestResult;
-import com.qwazr.database.model.TableStatus;
-import com.qwazr.database.store.CollectorInterface;
-import com.qwazr.database.store.KeyStore;
-import com.qwazr.database.store.Query;
-import com.qwazr.database.store.Table;
-import com.qwazr.database.store.Tables;
+import com.qwazr.database.model.*;
+import com.qwazr.database.store.*;
 import com.qwazr.server.ServerException;
 import com.qwazr.utils.ExceptionUtils;
 import com.qwazr.utils.FileUtils;
@@ -35,204 +27,198 @@ import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
+import java.util.stream.Stream;
 
 public class TableManager {
 
-	private final ReadWriteLock rwl = ReadWriteLock.stamped();
+    private final ReadWriteLock rwl = ReadWriteLock.stamped();
 
-	private final ExecutorService executorService;
+    private final ExecutorService executorService;
 
-	private final Path tablesDirectory;
+    private final Path tablesDirectory;
 
-	private final TableServiceInterface service;
+    private final TableServiceInterface service;
 
-	public TableManager(final ExecutorService executorService, final Path tablesDirectory) throws ServerException {
-		this.executorService = executorService;
-		this.tablesDirectory = tablesDirectory;
-		service = new TableServiceImpl(this);
-	}
+    public TableManager(final ExecutorService executorService, final Path tablesDirectory) throws ServerException {
+        this.executorService = executorService;
+        this.tablesDirectory = tablesDirectory;
+        service = new TableServiceImpl(this);
+    }
 
-	public static Path checkTablesDirectory(final Path dataDirectory) throws IOException {
-		final Path tablesDirectory = dataDirectory.resolve(TableServiceInterface.SERVICE_NAME);
-		if (!Files.exists(tablesDirectory))
-			Files.createDirectory(tablesDirectory);
-		return tablesDirectory;
-	}
+    public static Path checkTablesDirectory(final Path dataDirectory) throws IOException {
+        final Path tablesDirectory = dataDirectory.resolve(TableServiceInterface.SERVICE_NAME);
+        if (!Files.exists(tablesDirectory))
+            Files.createDirectory(tablesDirectory);
+        return tablesDirectory;
+    }
 
-	public TableServiceInterface getService() {
-		return service;
-	}
+    public TableServiceInterface getService() {
+        return service;
+    }
 
-	private Table getTable(final String tableName) {
-		final Path tableDirectory = tablesDirectory.resolve(tableName);
-		if (!Files.exists(tableDirectory))
-			throw new ServerException(Response.Status.NOT_FOUND, "Table not found: " + tableName);
-		return Tables.getInstance(executorService, tableDirectory.toFile(), null);
-	}
+    private Table getTable(final String tableName) {
+        final Path tableDirectory = tablesDirectory.resolve(tableName);
+        if (!Files.exists(tableDirectory))
+            throw new ServerException(Response.Status.NOT_FOUND, "Table not found: " + tableName);
+        return Tables.getInstance(executorService, tableDirectory.toFile(), null);
+    }
 
-	public void createTable(final String tableName, final KeyStore.Impl storeImpl) throws IOException {
-		rwl.writeEx(() -> {
-			final Path tableDirectory = tablesDirectory.resolve(tableName);
-			if (Files.exists(tableDirectory))
-				throw new ServerException(Response.Status.CONFLICT, "The table already exists: " + tableName);
-			Files.createDirectory(tableDirectory);
-			if (!Files.exists(tableDirectory))
-				throw new ServerException(Response.Status.INTERNAL_SERVER_ERROR,
-						"The directory cannot be created: " + tableDirectory.toAbsolutePath().toString());
-			Tables.getInstance(executorService, tableDirectory.toFile(), storeImpl);
-		});
-	}
+    public void createTable(final String tableName, final KeyStore.Impl storeImpl) throws IOException {
+        rwl.writeEx(() -> {
+            final Path tableDirectory = tablesDirectory.resolve(tableName);
+            if (Files.exists(tableDirectory))
+                throw new ServerException(Response.Status.CONFLICT, "The table already exists: " + tableName);
+            Files.createDirectory(tableDirectory);
+            if (!Files.exists(tableDirectory))
+                throw new ServerException(Response.Status.INTERNAL_SERVER_ERROR,
+                        "The directory cannot be created: " + tableDirectory.toAbsolutePath().toString());
+            Tables.getInstance(executorService, tableDirectory.toFile(), storeImpl);
+        });
+    }
 
-	public SortedSet<String> getNameSet() {
-		return rwl.read(() -> {
-			final TreeSet<String> names = new TreeSet<>();
-			Files.list(tablesDirectory)
-					.filter(p -> Files.isDirectory(p))
-					.filter(p -> ExceptionUtils.bypass(() -> !Files.isHidden(p)))
-					.forEach(p -> names.add(p.getFileName().toString()));
-			return names;
-		});
-	}
+    public SortedSet<String> getNameSet() {
+        return rwl.read(() -> {
+            final TreeSet<String> names = new TreeSet<>();
+            try (final Stream<Path> stream = Files.list(tablesDirectory)) {
+                stream.filter(p -> Files.isDirectory(p))
+                        .filter(p -> ExceptionUtils.bypass(() -> !Files.isHidden(p)))
+                        .forEach(p -> names.add(p.getFileName().toString()));
+            }
+            return names;
+        });
+    }
 
-	public TableStatus getStatus(final String tableName) throws IOException {
-		return rwl.readEx(() -> {
-			final Table table = getTable(tableName);
-			final TableDefinition definition = new TableDefinition(table.getImplementation(), table.getColumns());
-			return new TableStatus(definition, table.getSize());
-		});
-	}
+    public TableStatus getStatus(final String tableName) throws IOException {
+        return rwl.readEx(() -> {
+            final Table table = getTable(tableName);
+            final TableDefinition definition = new TableDefinition(table.getImplementation(), table.getColumns());
+            return new TableStatus(definition, table.getSize());
+        });
+    }
 
-	public Map<String, ColumnDefinition> getColumns(final String tableName) throws IOException {
-		return rwl.readEx(() -> getTable(tableName).getColumns());
-	}
+    public Map<String, ColumnDefinition> getColumns(final String tableName) throws IOException {
+        return rwl.readEx(() -> getTable(tableName).getColumns());
+    }
 
-	public void setColumn(final String tableName, final String columnName, final ColumnDefinition columnDefinition)
-			throws IOException {
-		rwl.writeEx(() -> {
-			getTable(tableName).setColumn(columnName, columnDefinition);
-		});
-	}
+    public void setColumn(final String tableName, final String columnName, final ColumnDefinition columnDefinition)
+            throws IOException {
+        rwl.writeEx(() -> {
+            getTable(tableName).setColumn(columnName, columnDefinition);
+        });
+    }
 
-	public void removeColumn(final String tableName, final String columnName) throws IOException {
-		rwl.writeEx(() -> {
-			getTable(tableName).removeColumn(columnName);
-		});
-	}
+    public void removeColumn(final String tableName, final String columnName) throws IOException {
+        rwl.writeEx(() -> {
+            getTable(tableName).removeColumn(columnName);
+        });
+    }
 
-	public List<Object> getColumnTerms(final String tableName, final String columnName, final Integer start,
-			final Integer rows) throws IOException {
-		return rwl.readEx(() -> getTable(tableName).getColumnTerms(columnName, start == null ? 0 : start,
-				rows == null ? 10 : rows));
-	}
+    public List<Object> getColumnTerms(final String tableName, final String columnName, final Integer start,
+                                       final Integer rows) throws IOException {
+        return rwl.readEx(() -> getTable(tableName).getColumnTerms(columnName, start == null ? 0 : start,
+                rows == null ? 10 : rows));
+    }
 
-	public List<String> getColumnTermKeys(final String tableName, final String columnName, final String term,
-			final Integer start, final Integer rows) throws IOException {
-		return rwl.readEx(() -> getTable(tableName).getColumnTermKeys(columnName, term, start == null ? 0 : start,
-				rows == null ? 10 : rows));
-	}
+    public List<String> getColumnTermKeys(final String tableName, final String columnName, final String term,
+                                          final Integer start, final Integer rows) throws IOException {
+        return rwl.readEx(() -> getTable(tableName).getColumnTermKeys(columnName, term, start == null ? 0 : start,
+                rows == null ? 10 : rows));
+    }
 
-	public void deleteTable(final String tableName) throws IOException {
-		rwl.writeEx(() -> {
-			final Path tableDirectory = tablesDirectory.resolve(tableName);
-			if (!Files.exists(tableDirectory))
-				throw new ServerException(Response.Status.NOT_FOUND, "Table not found: " + tableName);
-			Tables.delete(tableDirectory.toFile());
-			FileUtils.deleteDirectory(tableDirectory);
-		});
-	}
+    public void deleteTable(final String tableName) throws IOException {
+        rwl.writeEx(() -> {
+            final Path tableDirectory = tablesDirectory.resolve(tableName);
+            if (!Files.exists(tableDirectory))
+                throw new ServerException(Response.Status.NOT_FOUND, "Table not found: " + tableName);
+            Tables.delete(tableDirectory.toFile());
+            FileUtils.deleteDirectory(tableDirectory);
+        });
+    }
 
-	public void upsertRow(final String tableName, final String row_id, final Map<String, Object> nodeMap)
-			throws IOException {
-		rwl.readEx(() -> getTable(tableName).upsertRow(row_id, nodeMap));
-	}
+    public void upsertRow(final String tableName, final String row_id, final Map<String, Object> nodeMap)
+            throws IOException {
+        rwl.readEx(() -> getTable(tableName).upsertRow(row_id, nodeMap));
+    }
 
-	public int upsertRows(final String tableName, final List<Map<String, Object>> rows) throws IOException {
-		return rwl.readEx(() -> getTable(tableName).upsertRows(rows));
-	}
+    public int upsertRows(final String tableName, final List<Map<String, Object>> rows) throws IOException {
+        return rwl.readEx(() -> getTable(tableName).upsertRows(rows));
+    }
 
-	public LinkedHashMap<String, Object> getRow(final String tableName, final String key, final Set<String> columns)
-			throws IOException {
-		return rwl.readEx(() -> {
-			final Table table = getTable(tableName);
-			final LinkedHashMap<String, Object> row = table.getRow(key, columns);
-			if (row == null)
-				throw new ServerException(Response.Status.NOT_FOUND, "Row not found: " + key);
-			return row;
-		});
-	}
+    public LinkedHashMap<String, Object> getRow(final String tableName, final String key, final Set<String> columns)
+            throws IOException {
+        return rwl.readEx(() -> {
+            final Table table = getTable(tableName);
+            final LinkedHashMap<String, Object> row = table.getRow(key, columns);
+            if (row == null)
+                throw new ServerException(Response.Status.NOT_FOUND, "Row not found: " + key);
+            return row;
+        });
+    }
 
-	public List<Map<String, Object>> getRows(final String tableName, final Set<String> columns, final Set<String> keys)
-			throws IOException {
-		return rwl.readEx(() -> {
-			final Table table = getTable(tableName);
-			final List<Map<String, Object>> rows = new ArrayList<>();
-			table.getRows(keys, columns, rows);
-			return rows;
-		});
-	}
+    public List<Map<String, Object>> getRows(final String tableName, final Set<String> columns, final Set<String> keys)
+            throws IOException {
+        return rwl.readEx(() -> {
+            final Table table = getTable(tableName);
+            final List<Map<String, Object>> rows = new ArrayList<>();
+            table.getRows(keys, columns, rows);
+            return rows;
+        });
+    }
 
-	public List<String> getPrimaryKeys(final String tableName, final Integer start, final Integer rows)
-			throws IOException {
-		return rwl.readEx(
-				() -> getTable(tableName).getPrimaryKeys(start == null ? 0 : start, rows == null ? 10 : rows));
-	}
+    public List<String> getPrimaryKeys(final String tableName, final Integer start, final Integer rows)
+            throws IOException {
+        return rwl.readEx(
+                () -> getTable(tableName).getPrimaryKeys(start == null ? 0 : start, rows == null ? 10 : rows));
+    }
 
-	public boolean deleteRow(final String tableName, final String key) throws IOException {
-		return rwl.readEx(() -> getTable(tableName).deleteRow(key));
-	}
+    public boolean deleteRow(final String tableName, final String key) throws IOException {
+        return rwl.readEx(() -> getTable(tableName).deleteRow(key));
+    }
 
-	public TableRequestResult query(final String tableName, final TableRequest request) throws IOException {
-		return rwl.readEx(() -> {
+    public TableRequestResult query(final String tableName, final TableRequest request) throws IOException {
+        return rwl.readEx(() -> {
 
-			final long start = request.start == null ? 0 : request.start;
-			final long rows = request.rows == null ? Long.MAX_VALUE : request.rows;
+            final long start = request.start == null ? 0 : request.start;
+            final long rows = request.rows == null ? Long.MAX_VALUE : request.rows;
 
-			final Table table = getTable(tableName);
+            final Table table = getTable(tableName);
 
-			final Map<String, Map<String, CollectorInterface.LongCounter>> counters;
-			if (request.counters != null && !request.counters.isEmpty()) {
-				counters = new LinkedHashMap<>();
-				request.counters.forEach(col -> counters.put(col, new HashMap<>()));
-			} else
-				counters = null;
+            final Map<String, Map<String, CollectorInterface.LongCounter>> counters;
+            if (request.counters != null && !request.counters.isEmpty()) {
+                counters = new LinkedHashMap<>();
+                request.counters.forEach(col -> counters.put(col, new HashMap<>()));
+            } else
+                counters = null;
 
-			final Query query = request.query == null ? null : Query.prepare(request.query, null);
+            final Query query = request.query == null ? null : Query.prepare(request.query, null);
 
-			final RoaringBitmap docBitset = table.query(query, counters).finalBitmap;
+            final RoaringBitmap docBitset = table.query(query, counters).finalBitmap;
 
-			if (docBitset == null || docBitset.isEmpty())
-				return new TableRequestResult(0L, Collections.emptyList(), Collections.emptyMap());
+            if (docBitset == null || docBitset.isEmpty())
+                return new TableRequestResult(0L, Collections.emptyList(), Collections.emptyMap());
 
-			final TableRequestResult.Builder builder =
-					new TableRequestResult.Builder((long) docBitset.getCardinality());
+            final TableRequestResult.Builder builder =
+                    new TableRequestResult.Builder((long) docBitset.getCardinality());
 
-			final List<Map<String, Object>> resultRows = new ArrayList<>();
-			table.getRows(docBitset, request.columns, start, rows, resultRows);
-			builder.rows(resultRows);
+            final List<Map<String, Object>> resultRows = new ArrayList<>();
+            table.getRows(docBitset, request.columns, start, rows, resultRows);
+            builder.rows(resultRows);
 
-			if (counters == null)
-				return builder.build();
+            if (counters == null)
+                return builder.build();
 
-			final Map<String, Map<String, Long>> resultCounters = new LinkedHashMap<>();
-			counters.forEach((countersEntryKey, countersEntry) -> {
-				final LinkedHashMap<String, Long> counter = new LinkedHashMap<>();
-				countersEntry.forEach((key, counterEntry) -> counter.put(key, counterEntry.count));
-				resultCounters.put(countersEntryKey, counter);
-			});
-			builder.counters(resultCounters);
+            final Map<String, Map<String, Long>> resultCounters = new LinkedHashMap<>();
+            counters.forEach((countersEntryKey, countersEntry) -> {
+                final LinkedHashMap<String, Long> counter = new LinkedHashMap<>();
+                countersEntry.forEach((key, counterEntry) -> counter.put(key, counterEntry.count));
+                resultCounters.put(countersEntryKey, counter);
+            });
+            builder.counters(resultCounters);
 
-			return builder.build();
-		});
-	}
+            return builder.build();
+        });
+    }
 
 }
