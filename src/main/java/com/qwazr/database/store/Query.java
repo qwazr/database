1899c5baacdebe863c3b7f57bf43af2fd6a2ace0
Emmanuel Keller <ekeller@open-search-server.com>
2019-03-23 12:38:15 +0000

    Replace RummablePool by TaskPool

--- src/main/java/com/qwazr/database/store/Query.java
+++ src/main/java/com/qwazr/database/store/Query.java
@@ -16,9 +16,7 @@
 package com.qwazr.database.store;
 
 import com.qwazr.database.model.TableQuery;
-import com.qwazr.server.ServerException;
-import com.qwazr.utils.concurrent.RunnablePool;
-import com.qwazr.utils.concurrent.RunnablePoolException;
+import com.qwazr.utils.concurrent.TaskPool;
 import org.roaringbitmap.RoaringBitmap;
 
 import java.io.IOException;
@@ -31,153 +29,154 @@ import java.util.function.BiConsumer;
 
 public interface Query {
 
-	static Query prepare(final TableQuery query, final QueryHook queryHook) throws QueryException {
-		final Query newQuery;
-		if (query instanceof TableQuery.Group) {
-			if (query instanceof TableQuery.Or)
-				newQuery = new OrGroup((TableQuery.Or) query, queryHook);
-			else if (query instanceof TableQuery.And)
-				newQuery = new AndGroup((TableQuery.And) query, queryHook);
-			else
-				newQuery = null;
-		} else if (query instanceof TableQuery.Term) {
-			if (query instanceof TableQuery.StringTerm)
-				newQuery = new TermQuery<>((TableQuery.StringTerm) query);
-			else if (query instanceof TableQuery.LongTerm)
-				newQuery = new TermQuery<>((TableQuery.LongTerm) query);
-			else if (query instanceof TableQuery.IntegerTerm)
-				newQuery = new TermQuery<>((TableQuery.IntegerTerm) query);
-			else if (query instanceof TableQuery.DoubleTerm)
-				newQuery = new TermQuery<>((TableQuery.DoubleTerm) query);
-			else if (query instanceof TableQuery.FloatTerm)
-				newQuery = new TermQuery<>((TableQuery.FloatTerm) query);
-			else
-				newQuery = null;
-		} else
-			newQuery = null;
-		if (newQuery == null)
-			throw new QueryException("Unknown query type: " + query);
-		if (queryHook != null)
-			queryHook.query(newQuery);
-		return newQuery;
-	}
+    static Query prepare(final TableQuery query, final QueryHook queryHook) throws QueryException {
+        final Query newQuery;
+        if (query instanceof TableQuery.Group) {
+            if (query instanceof TableQuery.Or)
+                newQuery = new OrGroup((TableQuery.Or) query, queryHook);
+            else if (query instanceof TableQuery.And)
+                newQuery = new AndGroup((TableQuery.And) query, queryHook);
+            else
+                newQuery = null;
+        } else if (query instanceof TableQuery.Term) {
+            if (query instanceof TableQuery.StringTerm)
+                newQuery = new TermQuery<>((TableQuery.StringTerm) query);
+            else if (query instanceof TableQuery.LongTerm)
+                newQuery = new TermQuery<>((TableQuery.LongTerm) query);
+            else if (query instanceof TableQuery.IntegerTerm)
+                newQuery = new TermQuery<>((TableQuery.IntegerTerm) query);
+            else if (query instanceof TableQuery.DoubleTerm)
+                newQuery = new TermQuery<>((TableQuery.DoubleTerm) query);
+            else if (query instanceof TableQuery.FloatTerm)
+                newQuery = new TermQuery<>((TableQuery.FloatTerm) query);
+            else
+                newQuery = null;
+        } else
+            newQuery = null;
+        if (newQuery == null)
+            throw new QueryException("Unknown query type: " + query);
+        if (queryHook != null)
+            queryHook.query(newQuery);
+        return newQuery;
+    }
 
-	RoaringBitmap execute(final QueryContext context, final ExecutorService executor) throws IOException;
+    RoaringBitmap execute(final QueryContext context, final ExecutorService executor) throws IOException;
 
-	class TermQuery<T> implements Query {
+    class TermQuery<T> implements Query {
 
-		private volatile String field;
+        private volatile String field;
 
-		private final T value;
+        private final T value;
 
-		public TermQuery(final TableQuery.Term<T> termQuery) {
-			Objects.requireNonNull(termQuery, "The term query is null");
-			this.field = termQuery.column;
-			this.value = termQuery.value;
-		}
+        public TermQuery(final TableQuery.Term<T> termQuery) {
+            Objects.requireNonNull(termQuery, "The term query is null");
+            this.field = termQuery.column;
+            this.value = termQuery.value;
+        }
 
-		final public String getField() {
-			return field;
-		}
+        final public String getField() {
+            return field;
+        }
 
-		final public void setField(String newField) {
-			this.field = newField;
-		}
+        final public void setField(String newField) {
+            this.field = newField;
+        }
 
-		@Override
-		final public RoaringBitmap execute(final QueryContext context, final ExecutorService executor)
-				throws IOException {
-			final RoaringBitmap bitset = context.getIndexedBitset(field, value);
-			if (bitset == null)
-				return new RoaringBitmap();
-			return bitset;
-		}
+        @Override
+        final public RoaringBitmap execute(final QueryContext context, final ExecutorService executor)
+                throws IOException {
+            final RoaringBitmap bitset = context.getIndexedBitset(field, value);
+            if (bitset == null)
+                return new RoaringBitmap();
+            return bitset;
+        }
 
-		final public T getValue() {
-			return value;
-		}
-	}
+        final public T getValue() {
+            return value;
+        }
+    }
 
-	abstract class GroupQuery implements Query {
+    abstract class GroupQuery implements Query {
 
-		private final Collection<Query> queries;
+        private final Collection<Query> queries;
 
-		protected GroupQuery(TableQuery.Group groupQuery, QueryHook queryHook) throws QueryException {
-			queries = new ArrayList<>(groupQuery.queries.size());
-			groupQuery.queries.forEach(n -> this.queries.add(Query.prepare(n, queryHook)));
-		}
+        protected GroupQuery(TableQuery.Group groupQuery, QueryHook queryHook) throws QueryException {
+            queries = new ArrayList<>(groupQuery.queries.size());
+            groupQuery.queries.forEach(n -> this.queries.add(Query.prepare(n, queryHook)));
+        }
 
-		final public void add(Query query) {
-			queries.add(query);
-		}
+        final public void add(Query query) {
+            queries.add(query);
+        }
 
-		final RoaringBitmap execute(final QueryContext context, final ExecutorService executor,
-				final BiConsumer<RoaringBitmap, RoaringBitmap> reduce) throws IOException {
-			try (final RunnablePool<RoaringBitmap> runnablePool = new RunnablePool<>(executor)) {
-				final RoaringBitmap finalBitmap = new RoaringBitmap();
-				queries.forEach(query -> runnablePool.submit(() -> {
-					final RoaringBitmap bitmap = query.execute(context, executor);
-					if (bitmap != null) {
-						synchronized (finalBitmap) {
-							reduce.accept(bitmap, finalBitmap);
-						}
-					}
-					return bitmap;
-				}));
-				return finalBitmap;
-			} catch (RunnablePoolException e) {
-				throw ServerException.of(e);
-			}
-		}
-	}
+        final RoaringBitmap execute(final QueryContext context, final ExecutorService executor,
+                                    final BiConsumer<RoaringBitmap, RoaringBitmap> reduce) {
+            try (final TaskPool runnablePool = TaskPool.of(executor)) {
+                final RoaringBitmap finalBitmap = new RoaringBitmap();
+                queries.forEach(query -> runnablePool.submit(() -> {
+                    final RoaringBitmap bitmap;
+                    try {
+                        bitmap = query.execute(context, executor);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (bitmap != null) {
+                        synchronized (finalBitmap) {
+                            reduce.accept(bitmap, finalBitmap);
+                        }
+                    }
+                    return bitmap;
+                }));
+                return finalBitmap;
+            }
+        }
+    }
 
-	final class OrGroup extends GroupQuery {
+    final class OrGroup extends GroupQuery {
 
-		protected OrGroup(final TableQuery.Or orQuery, QueryHook queryHook) {
-			super(orQuery, queryHook);
-		}
+        protected OrGroup(final TableQuery.Or orQuery, QueryHook queryHook) {
+            super(orQuery, queryHook);
+        }
 
-		@Override
-		final public RoaringBitmap execute(final QueryContext context, final ExecutorService executor)
-				throws IOException {
-			return execute(context, executor, (bitmap, finalBitmap) -> finalBitmap.or(bitmap));
-		}
-	}
+        @Override
+        final public RoaringBitmap execute(final QueryContext context, final ExecutorService executor) {
+            return execute(context, executor, (bitmap, finalBitmap) -> finalBitmap.or(bitmap));
+        }
+    }
 
-	class AndGroup extends GroupQuery {
+    class AndGroup extends GroupQuery {
 
-		protected AndGroup(final TableQuery.And andQuery, final QueryHook queryHook) {
-			super(andQuery, queryHook);
-		}
+        protected AndGroup(final TableQuery.And andQuery, final QueryHook queryHook) {
+            super(andQuery, queryHook);
+        }
 
-		@Override
-		final public RoaringBitmap execute(final QueryContext context, final ExecutorService executor)
-				throws IOException {
-			final AtomicBoolean first = new AtomicBoolean(true);
-			return execute(context, executor, (bitmap, finalBitmap) -> {
-				if (first.getAndSet(false))
-					finalBitmap.or(bitmap);
-				else
-					finalBitmap.and(bitmap);
-			});
-		}
-	}
+        @Override
+        final public RoaringBitmap execute(final QueryContext context, final ExecutorService executor) {
+            final AtomicBoolean first = new AtomicBoolean(true);
+            return execute(context, executor, (bitmap, finalBitmap) -> {
+                if (first.getAndSet(false))
+                    finalBitmap.or(bitmap);
+                else
+                    finalBitmap.and(bitmap);
+            });
+        }
+    }
 
-	class QueryException extends RuntimeException {
+    class QueryException extends RuntimeException {
 
-		/**
-		 *
-		 */
-		private static final long serialVersionUID = -5566235355622756480L;
+        /**
+         *
+         */
+        private static final long serialVersionUID = -5566235355622756480L;
 
-		private QueryException(final String reason) {
-			super(reason);
-		}
-	}
+        private QueryException(final String reason) {
+            super(reason);
+        }
+    }
 
-	interface QueryHook {
+    interface QueryHook {
 
-		void query(Query query);
-	}
+        void query(Query query);
+    }
 
 }
